c438eb1d4f0c46818b2cad1101d9e602e786e4ec
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-06-17 19:55:42 -0300

    commit code from pair programming with Roberto week 5

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,10 +1,11 @@
 const chai = require('chai');
 const assert = chai.assert;
 
+
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if FRANK is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +21,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true SEINFELD word is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +36,55 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  
+  it("should return false JOE word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'JOE')
+
+    assert.isFalse(result);
+  });
+
+  it("should return true LARRY word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRY')
+
+    assert.isTrue(result);
+  });
+
+
+  it("should return true when PAW is present", function() {
+    const result = wordSearch([
+        ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+        ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+        ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+        ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+        ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+        ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+        ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+        ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+        ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+      ], 'PAW')
+
+      assert.isTrue(result);
+  });
+
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,66 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    let newMatrix = [];
+    let tempArray = [];
+    const yLength = letters.length;
+    const xLength = letters[0].length;
+    
+    for (let x = 0; x < xLength; x++){
+        for (let y = 0; y < yLength; y++){
+        tempArray.push(letters[y][x]);
+        }
+        newMatrix.push(tempArray);
+        tempArray = [];
+    } 
+
+    const verticalJoin = newMatrix.map(lx => lx.join(''))
+    //console.log(verticalJoin)
+
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+
+    
+
+    let tempBackArray = [];
+    let backwardArray = [];
+    
+    //console.log(xLength, yLength);
+    for(let y = 0; y <= yLength - 1; y++ ){
+        //console.log(`this is y: ${letters[y]}`)
+        for (let x = xLength -1; x >= 0; x--){
+            //console.log(`this is x: ${x}`)
+            tempBackArray.push(letters[y][x]);
+        }
+        backwardArray.push(tempBackArray);
+        tempBackArray = [];
+    } //console.log(backwardArray);
+
+    const backwardJoin = backwardArray.map(lb => lb.join(''))
+    
+    for (l of backwardJoin) {
+        if (l.includes(word)) return true
+    }
+
+
+    return false;
+
 }
+
+
+// wordSearch([
+//     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//   ], 'PAW')
+
 
 module.exports = wordSearch
